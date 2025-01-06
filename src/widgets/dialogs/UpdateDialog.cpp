51baa3883980e20743c4ddd08f4b796e557227c1
Felanbird <41973452+Felanbird@users.noreply.github.com>
2025-01-06 15:09:44 -0500

    y

--- src/widgets/dialogs/UpdateDialog.cpp
+++ src/widgets/dialogs/UpdateDialog.cpp
@@ -77,7 +77,7 @@ void UpdateDialog::updateStatusChanged(Updates::Status status)
             this->ui_.label->setText(
                 "Downloading updates.\n\nChatterino will restart "
                 "automatically when the download is done.");
-            label->setWordWrap(true)
+            this->ui_.label->setWordWrap(true)
         }
         break;
 
